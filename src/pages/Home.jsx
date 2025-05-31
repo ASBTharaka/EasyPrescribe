4d51e667f14172f32ab16d4514ce3fb6f4e7b2ec
ASBTharaka <tharakarathnayake6@gmail.com>
2025-05-31 23:50:49 +0530

    second_day1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import Header from '../components/Header'
 import Specialitymenu from '../components/Specialitymenu'
-<<<<<<< HEAD:frontend/src/pages/Home.jsx
-=======
+
 import TopDoctors from '../components/TopDoctors'
->>>>>>> 73ae7c9 (Second_day):src/pages/Home.jsx
 
 const Home = () => {
   return (
     <div>
         <Header/>
         <Specialitymenu/>
-<<<<<<< HEAD:frontend/src/pages/Home.jsx
-=======
+
         <TopDoctors/>
->>>>>>> 73ae7c9 (Second_day):src/pages/Home.jsx
+
     </div>
   )
 }
